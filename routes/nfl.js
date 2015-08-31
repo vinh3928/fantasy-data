350ba00a352ec9f4d16062291bbd57370edb3f85
Vinh Nguyen <vnguyen.cheme@gmail.com>
2015-08-31 16:24:25 -0600

    added player route

--- routes/nfl.js
+++ routes/nfl.js
@@ -3,14 +3,23 @@ var express = require('express');
 var router = express.Router();
 var axios = require("axios");
 
-router.get('/players/:date', function(req, res, next) {
+router.get('/allplayers/:date', function(req, res, next) {
   axios({
     method: "get",
     url: "http://api.nfldata.apiphany.com/nfl/v2/JSON/PlayerSeasonStats/"+ req.params.date + "REG",
     headers: {"Ocp-Apim-Subscription-Key": process.env.FANTASY_NFL_KEY}
   }).then(function(data) {
-    res.status(202).json(data);
+    res.status(200).json(data);
   });
 });
 
+router.get('/player/:id', function(req, res, next) {
+  axios({
+    method: "get",
+    url: "http://api.nfldata.apiphany.com/nfl/v2/JSON/Player/"+ req.params.id,
+    headers: {"Ocp-Apim-Subscription-Key": process.env.FANTASY_NFL_KEY}
+  }).then(function(data) {
+    res.status(200).json(data);
+  });
+});
 module.exports = router;
